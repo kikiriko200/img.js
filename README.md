9297f3b83b89450f27643850205ea3ade7fcd920
kikiriko200 <fujitakakikiriko200@yahoo.co.jp>
2019-03-17 12:52:27 +0900

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # img.js
-Resize images square
+画像を正方形にリサイズします。
+
+## 説明
+
+画像をより短い辺に合わせる形で正方形にリサイズします。
+
+これによって、`border-radius:50%`が綺麗な真円になります。
+
+## 変数
+
+|名前 |説明 |
+|---|---|
+|`scale` |拡大率(デフォルト:1)|
+
+## CDN
+https://cdn.jsdelivr.net/gh/kikiriko200/img.js/img.js
+https://cdn.jsdelivr.net/gh/kikiriko200/img.js/img.min.js
+
+## ライセンス
+MIT License
+
+&copy;2019- [DAFU](https://github.com/kikiriko200/)
---
+++ img.js
@@ -0,0 +1,22 @@
+//img.js
+//(c) 2019- DAFU(https://github.com/kikiriko200/img.js)
+//MIT License(https://github.com/kikiriko200/img.js/blob/master/LICENSE)
+var counter = 0; //個別番号
+var scale = 1; // 倍率
+$('.as').each(function(){
+	var height = $(this).parent();
+	height = height.height();
+	var width = $(this).parent();
+	width = width.width();
+	if (height <= width){ //小さい方に合わせる
+		width = height;
+	}else{
+		height = width;
+	}
+	$(this).css({ //CSS制御
+		'height': height*scale,
+		'width' : width*scale
+  });
+  counter++;
+  console.log('Resize Image#'+counter+'\nwidth:'+width+'px;\nheight:'+height+'px');
+});
